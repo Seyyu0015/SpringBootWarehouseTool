760cbe0a958debb6986d2805f4c8404275b4023b
Seyyu0015 <seyyu0015@163.com>
2022-09-10 14:58:51 +0800

    Initial commit

--- src/main/java/com/example/common/Const.java
+++ src/main/java/com/example/common/Const.java
@@ -7,20 +7,4 @@ package com.example.common;
  */
 public interface Const {
 
-    int SERVER_PORT=4006;
-
-    /**
-     * 本地服务器的根地址
-     */
-    String BASE_URL="http://localhost:"+SERVER_PORT;
-
-    /**
-     * 录播图的路径
-     */
-    String IMAGE_URL="/images";
-
-    /**
-     * 图片基路径
-     */
-    String LINK_BASE_URL="http://iwenwiki.com/api/livable";
 }
